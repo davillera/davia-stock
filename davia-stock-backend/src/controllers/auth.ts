929f23d2a00ada5942b72c4f2b9f84b5bf14cd08
Daniel Villera <davillera77@gmail.com>
2024-11-12 07:48:10 -0500

    changepassword

--- davia-stock-backend/src/controllers/auth.ts
+++ davia-stock-backend/src/controllers/auth.ts
@@ -76,3 +76,41 @@ export const login = async (req: Request, res: Response): Promise<Response> => {
 		return res.status(500).json({ message: "Error al autenticar al usuario.", error });
 	}
 };
+
+export const changePassword = async (req: Request, res: Response) => {
+
+	const { email, oldPassword, newPassword } = req.body;
+
+	if (!oldPassword || !newPassword) {
+		return res.status(400).json({ message: "La contraseña actual y la nueva son requeridas." });
+	}
+
+	if (newPassword.length < 6) {
+		return res.status(400).json({ message: "La nueva contraseña debe tener al menos 6 caracteres." });
+	}
+	
+	try{
+		const userRepository = AppDataSource.getRepository(User);
+		const user = await userRepository.findOne({ where: { email } });
+
+		if (!user) {
+			return res.status(401).json({
+				message: "El usuario no existe."
+			});
+		}
+
+		const isOldPasswordValid = await bcrypt.compare(oldPassword, user.password);
+
+		if (!isOldPasswordValid) {
+			return res.status(401).json({ message: "La contraseña actual es incorrecta." });
+		}
+
+		user.password = await bcrypt.hash(newPassword, 10);
+		await userRepository.save(user);
+
+		return res.status(200).json({ message: "Contraseña actualizada correctamente." });
+
+	} catch (error){
+		return res.status(500).json({ message: "Error al cambiar la contraseña.", error });
+	}
+}
